3e0d7e027e04a0a0274d9cdc2bcc0ad269f907ba
Julien Bidart <jbidart@ncsu.edu>
2020-05-18 18:08:10 +0200

    Set up devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-  has_many :stars
 
-  validates :nickname, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50}
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50}
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+ 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :stars
+  validates :nickname, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50} 
 end
