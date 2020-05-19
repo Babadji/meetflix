57795ac331243192b79662d60c611b6bfee726b3
Hippolyte Cochin <hippolyte.cochin@gmail.com>
2020-05-19 13:01:39 +0200

    new seed and stars index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,49 @@
-
-Star.destroy_all
-
-# Stars
-cersei = Star.create(name: 'Cersei', category: 'GoT', price: 100000)
-
-tommy = Star.create(name: 'Thomas Shelby', category: 'Peaky Blinders', price: 50000)
-
-tokyo = Star.create(name: 'Tokyo', category: 'Casa del Papel', price: 25000)
-
-batman = Star.create(name: 'Batman', category: 'The Batman', price: 150000)
-
 # Users
 
-puts "#{cersei.name} created"
-
-puts "#{tommy.name} created"
-
-puts "#{tokyo.name} created"
-
-puts "#{batman.name} created"
-
-puts 'Creating users...'
 User.destroy_all
-jumaru = User.create(nickname: 'Jumaru', email: "chilling@gmail.com")
-emil = User.create(nickname: "Mimil", email: "campagne@gmail.com")
-hippo = User.create(nickname: "Popotame", email: "cruising@gmail.com")
-claire = User.create(nickname: "babadji", email: "exquis@hotmail.fr")
-
+Star.destroy_all
+puts 'Creating users...'
+jumaru = User.create(nickname: 'Jumaru', email: "chilling@gmail.com", password: "123456")
+emil = User.create(nickname: "Mimil", email: "campagne@gmail.com", password: "123456")
+hippo = User.create(nickname: "Popotame", email: "cruising@gmail.com", password: "123456")
+claire = User.create(nickname: "babadji", email: "exquis@hotmail.fr", password: "123456")
+obama = User.create(nickname: "babama", email: "president@yahoo.com", password: "123456")
 puts 'Success'
+puts 'Creating characters (stars)...'
+# # Stars
+
+cersei = Star.new(name: 'Cersei', category: 'GoT', price: 50)
+cersei.user = jumaru
+cersei.save!
+tommy = Star.new(name: 'Thomas Shelby', category: 'Peaky Blinders', price: 45)
+tommy.user = emil
+tommy.save!
+tokyo = Star.new(name: 'Tokyo', category: 'Casa del Papel', price: 75)
+tokyo.user = hippo
+tokyo.save!
+batman = Star.new(name: 'Batman', category: 'The Batman', price: 89,)
+batman.user = claire
+batman.save!
+naruto = Star.new(name: 'Naruto', category: 'Naruto', price: 85)
+naruto.user = hippo
+naruto.save!
+troy = Star.new(name: 'Troy Barnes', category: 'Community', price: 10)
+troy.user = obama
+troy.save!
+barney = Star.new(name: 'Barney Stinson', category: 'How I met your mother', price: 90)
+barney.user = emil
+barney.save!
+rick = Star.new(name: 'Rick', category: 'Rick and Morty', price: 60)
+rick.user = jumaru
+rick.save!
+puts 'Success'
+
+
+
+
+puts User.last
+puts Star.last
+
+# More seeds to come when other models are created.
+puts 'Seeding complete'
+
