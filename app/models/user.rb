48775d5337bd19060719ffabdbec7fa86e66c829
claire abadji <claire.abadji@gmail.com>
2020-05-19 14:37:39 +0200

    change design

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
 
- 
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :stars
-  validates :nickname, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50} 
+  # validates :nickname, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50}
 end
