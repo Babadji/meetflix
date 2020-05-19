b27886501ee1a05ae69a7d0a1049ab8e41fe0761
claire abadji <claire.abadji@gmail.com>
2020-05-19 12:25:35 +0200

    pundit V1

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,10 +1,52 @@
 class StarsController < ApplicationController
 
   def index
-    @stars = Star.all
+    @stars = policy_scope(Star)
   end
 
   def show
     @star = Star.find(params[:id])
+  end
+
+  def new
+    @star = Star.new
+    authorize @star
+  end
+
+  def edit
+  end
+
+  def create
+    @star = Star.new(star_params)
+    @star.user = current_user
+    authorize @star
+    if star.save
+      redirect_to @star, notice: "Star has been succesfully created"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @star.update(star_params)
+      redirect_to @star, notice: "Star has been succesfully updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @star.destroy
+    redirect_to stars_url, notice: "Star has been succesfully destroyed"
+  end
+
+  private
+
+  def set_star
+    @star = Star.find(params[:id])
+  end
+
+  def star_params
+    params.require(:star).permit(:name)
   end
 end
