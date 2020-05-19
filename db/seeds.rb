4d2a5aec47d312597968f92af8ef89fc0b087480
Julien Bidart <jbidart@ncsu.edu>
2020-05-19 11:54:37 +0200

    SEED NOT WORKING/ But code fix for VIEWS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,8 @@
 
-Star.destroy_all
 
-# Stars
-cersei = Star.create(name: 'Cersei', category: 'GoT', price: 100000)
-
-tommy = Star.create(name: 'Thomas Shelby', category: 'Peaky Blinders', price: 50000)
-
-tokyo = Star.create(name: 'Tokyo', category: 'Casa del Papel', price: 25000)
-
-batman = Star.create(name: 'Batman', category: 'The Batman', price: 150000)
 
 # Users
 
-puts "#{cersei.name} created"
-
-puts "#{tommy.name} created"
-
-puts "#{tokyo.name} created"
-
-puts "#{batman.name} created"
 
 puts 'Creating users...'
 User.destroy_all
@@ -26,5 +10,30 @@ jumaru = User.create(nickname: 'Jumaru', email: "chilling@gmail.com")
 emil = User.create(nickname: "Mimil", email: "campagne@gmail.com")
 hippo = User.create(nickname: "Popotame", email: "cruising@gmail.com")
 claire = User.create(nickname: "babadji", email: "exquis@hotmail.fr")
+obama = User.create(nickname: "babama", email: "president@yahoo.com")
 
 puts 'Success'
+
+puts 'Creating characters (stars)...'
+Star.destroy_all
+# Stars
+cersei = Star.create(name: 'Cersei', category: 'GoT', price: 50, user: jumaru)
+
+tommy = Star.create(name: 'Thomas Shelby', category: 'Peaky Blinders', price: 45, user: emil)
+
+tokyo = Star.create(name: 'Tokyo', category: 'Casa del Papel', price: 75, user: hippo)
+
+batman = Star.create(name: 'Batman', category: 'The Batman', price: 89, user: claire)
+
+naruto = Star.create(name: 'Naruto', category: 'Naruto', price: 85, user: obama)
+
+troy = Star.create(name: 'Troy Barnes', category: 'Community', price: 10, user: emil)
+
+barney = Star.create(name: 'Barney Stinson', category: 'How I met your mother', price: 90, user: hippo)
+
+rick = Star.create(name: 'Rick', category: 'Rick and Morty', price: 60, user: claire)
+puts 'Success'
+
+
+# More seeds to come when other models are created. 
+puts 'Seeding complete'
