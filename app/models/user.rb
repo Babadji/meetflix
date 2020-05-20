1b85ab8a52f433f566488630c4a3b885e62877b3
claire abadji <claire.abadji@gmail.com>
2020-05-20 14:31:19 +0200

    change description seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  has_many :reservations
+
+  has_many :reservations, dependent: :destroy
 
   has_many :stars, dependent: :destroy
 
