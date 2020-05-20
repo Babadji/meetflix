65a9488e7a25b5fe3ba4200e7613c2286aabf54a
claire abadji <claire.abadji@gmail.com>
2020-05-20 13:02:37 +0200

    home issues

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,16 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @stars = Star.all
+  end
+
+  def index
+    @stars = policy_scope(Star)
+    @stars = Star.all
+  end
+
+  def show
+    @star = Star.find(params[:id])
+    authorize @star
   end
 end
