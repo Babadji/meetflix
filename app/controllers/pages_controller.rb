84ee482d7fea5df9ffc9be4e0347760ba9c5fb82
Hippolyte Cochin <hippolyte.cochin@gmail.com>
2020-05-21 12:22:20 +0200

    search form add

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,18 +2,25 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
 
-
-  def home
-    @stars = Star.all
-  end
-
   def index
     @stars = policy_scope(Star)
     @stars = Star.all
   end
 
+  def home
+    if params[:query].present?
+      sql_query = "name ILIKE :query OR category ILIKE :query"
+      @stars = Star.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @stars = Star.all
+    end
+  end
+
+
+
   def show
     @star = Star.find(params[:id])
     authorize @star
   end
+
 end
