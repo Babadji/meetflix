8f66aa586f7184a369acee9198a109fda6145afa
claire abadji <claire.abadji@gmail.com>
2020-05-21 14:49:00 +0200

    correction des contrôleurs + ajout de routes user + création page dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :stars, except: [:index, :show]
+  end
   resources :stars, only: [:index, :show] do
   resources :reservations, only: [:show, :new, :create]
   end
