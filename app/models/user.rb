1f4f1ca4d664ef43559eacdff29c2ae35ef29f35
Emile Prieur <emileprieur@gmail.com>
2020-05-22 12:28:00 +0200

    Add new SEED (photo and emil -> emile) + Avatar current_user to navbar and show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,11 @@
 class User < ApplicationRecord
-
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_many :reservations, dependent: :destroy
-
   has_many :stars, dependent: :destroy
+  has_one_attached :photo
 
   # validates :nickname, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,27 @@ require "open-uri"
 User.destroy_all
 Star.destroy_all
 puts 'Creating users...'
-jumaru = User.create(nickname: 'Jumaru', email: "chilling@gmail.com", password: "123456")
-emil = User.create(nickname: "Mimil", email: "campagne@gmail.com", password: "123456")
-hippo = User.create(nickname: "Popotame", email: "cruising@gmail.com", password: "123456")
+
+jumaru = User.new(nickname: 'Jumaru', email: "chilling@gmail.com", password: "123456")
+jumaru_photo = URI.open('https://res.cloudinary.com/dnzggrpqe/image/upload/v1590140211/Juju_adphpl.png')
+jumaru.photo.attach(io: jumaru_photo, filename: 'jumaru_photo.png', content_type: 'image/png')
+jumaru.save!
+
+emile = User.new(nickname: "Mimil", email: "campagne@gmail.com", password: "123456")
+emile_photo = URI.open('https://res.cloudinary.com/dnzggrpqe/image/upload/v1590140226/Mimile_ssfwgk.png')
+emile.photo.attach(io: emile_photo, filename: 'emile_photo.png', content_type: 'image/png')
+emile.save!
+
+hippo = User.new(nickname: "Popotame", email: "cruising@gmail.com", password: "123456")
+hippo_photo = URI.open('https://res.cloudinary.com/dnzggrpqe/image/upload/v1590140251/Hippo_zkbgqf.png')
+hippo.photo.attach(io: hippo_photo, filename: 'hippo_photo.png', content_type: 'image/png')
+hippo.save!
+
 claire = User.create(nickname: "babadji", email: "exquis@hotmail.fr", password: "123456")
+claire_photo = URI.open('https://res.cloudinary.com/dnzggrpqe/image/upload/v1590140239/Claire_rqfnmc.png')
+claire.photo.attach(io: claire_photo, filename: 'claire_photo.png', content_type: 'image/png')
+claire.save!
+
 obama = User.create(nickname: "babama", email: "president@yahoo.com", password: "123456")
 puts 'Success'
 puts 'Creating characters (stars)...'
@@ -22,7 +39,7 @@ cersei.address = '97 boulevard arago, Paris'
 cersei.save!
 
 tommy = Star.new(name: 'Thomas Shelby', category: 'Peaky Blinders', price: 45, description: "Deal with Mr Shelby." )
-tommy.user = emil
+tommy.user = emile
 tommy_photo = URI.open('https://res.cloudinary.com/dnzggrpqe/image/upload/v1589926470/peakyblinders3003a_zh1xex.jpg')
 tommy.photo.attach(io: tommy_photo, filename: 'tommy_photo.jpg', content_type: 'image/jpg')
 tommy.address = 'Chamberlain Square, Birmingham B3 3DH, Royaume-Uni'
@@ -64,8 +81,8 @@ puts 'Success'
 
 
 
-puts User.last
-puts Star.last
+puts User.last.nickname
+puts Star.last.name
 
 # More seeds to come when other models are created.
 
